9265baa3159b43762bd02ad30d6411b6fc978d07
Izabela-Alexandra OPREA (66868) <izabela.oprea@stud.acs.upb.ro>
2021-04-15 09:09:53 +0300

    end of B

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -3,6 +3,13 @@ package ro.pub.cs.systems.eim.practicaltest01var04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.CheckBox;
+
 
 public class PracticalTest01Var04MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class PracticalTest01Var04MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var04_main);
+
+        Button displayInfo = (Button) findViewById(R.id.display_button);
+        displayInfo.setOnClickListener(buttonClickListener);
     }
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            EditText userNameEditText = (EditText) findViewById(R.id.user_name_edit_text);
+            TextView greetingTextView = (TextView) findViewById(R.id.greeting_text_view);
+            EditText groupEditText = (EditText) findViewById(R.id.group_edit_text);
+            CheckBox nameCheckBox = (CheckBox) findViewById(R.id.check_name_checkbox);
+            CheckBox groupCheckBox = (CheckBox) findViewById(R.id.check_group_checkbox);
+            greetingTextView.setAlpha(1);
+
+            if (nameCheckBox.isChecked() && groupCheckBox.isChecked()) {
+                greetingTextView.setText(userNameEditText.getText().toString() + "," + groupEditText.getText().toString());
+            } else if (nameCheckBox.isChecked()) {
+                greetingTextView.setText(userNameEditText.getText().toString());
+
+            } else if ((groupCheckBox.isChecked())) {
+                greetingTextView.setText(groupCheckBox.getText().toString());
+            }
+
+
+        }
+
+    }
+
 }
